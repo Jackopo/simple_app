83a34ae0c5b19aeefb73b86795d15ae830f6cf1d
Jacopo Ghisolfi <jacopo.ghisolfi@infopark.de>
2017-10-12 13:03:24 +0200

    creating login and logout - first step

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "routing to users" do
+describe "routing" do
 
   it "should routes /signup to users#new" do
     expect(get: "/signup").to route_to(
@@ -12,6 +12,20 @@ describe "routing to users" do
   it "should routes post /users to users#create" do
     expect(post: "/users").to route_to(
       controller: "users",
+      action: "create"
+    )
+  end
+
+  it "should routes get /login to sessions#new" do
+    expect(get "/login").to route_to(
+      controller: "sessions",
+      action: "new"
+    )
+  end
+
+  it "should routes post /login to sessions#create" do
+    expect(post "/login").to route_to(
+      controller: "sessions",
       action: "create"
     )
   end
